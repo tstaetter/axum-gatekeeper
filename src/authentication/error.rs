071d307690ea9af605be27f35b8fb485998a5569
tstaetter <thomas.staetter@gmail.com>
2025-01-14 20:10:28 +0100

    feat: added skeleton IntoResponse impls for error types

--- src/authentication/error.rs
+++ src/authentication/error.rs
@@ -1,2 +1,14 @@
+use axum::response::{IntoResponse, Response};
+
 #[derive(thiserror::Error, Debug)]
 pub enum AuthenticationError {}
+
+impl IntoResponse for AuthenticationError {
+    fn into_response(self) -> Response {
+        let response = match self {
+            _ => "",
+        };
+
+        response.into_response()
+    }
+}
--- src/verification/error.rs
+++ src/verification/error.rs
@@ -1,2 +1,14 @@
+use axum::response::{IntoResponse, Response};
+
 #[derive(thiserror::Error, Debug)]
 pub enum VerificationError {}
+
+impl IntoResponse for VerificationError {
+    fn into_response(self) -> Response {
+        let response = match self {
+            _ => "",
+        };
+
+        response.into_response()
+    }
+}
