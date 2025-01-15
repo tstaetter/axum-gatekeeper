26e0f7b1c60c732295a67bf99dd0d24d943e807a
tstaetter <thomas.staetter@gmail.com>
2025-01-15 09:53:43 +0100

    feat: updated model

--- src/model.rs
+++ src/model.rs
@@ -6,6 +6,19 @@ pub trait GateKeeperModel: Serialize + DeserializeOwned + Send + Sync {
     /// Return the model object's ID
     fn id(&self) -> uuid::Uuid;
 
-    /// Return the model object's personal secret
+    /// Return the model's personal secret
     fn secret(&self) -> &'static str;
+
+    /// Return the model's `first_name`
+    fn first_name(&self) -> &str;
+
+    /// Return the model's `last_name`
+    fn last_name(&self) -> &str;
+
+    /// Return the model's `email`
+    fn email(&self) -> &str;
+
+    #[cfg(feature = "verification")]
+    /// Get the models verification token
+    fn verification_token(&self) -> Option<crate::tokens::VerificationToken>;
 }
